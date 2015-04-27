1a3895c308c22361a4c0f03f89960b0800165f91
EvgeniVasilev <evgenivasilev1209@abv.bg>
2015-04-27 22:46:15 +0300

    Add url according to g-hub pages location

--- controllers/person-details.js
+++ controllers/person-details.js
@@ -15,7 +15,7 @@ function personalDetails($scope, $window) {
 
 
 	$scope.change = function () {
-		$window.location = "./index.html#/" + $scope.lng;
+		$window.location = "./Resume/#/" + $scope.lng;
 
 
 		if ($scope.lng === "bg") {

4f8ff494d0b9a18cbde6de8a76087d9097b29ff2
EvgeniVasilev <evgenivasilev1209@abv.bg>
2015-05-07 20:35:14 +0300

    Add angular $http service to prject and order file structure

--- controllers/bg-translate.js
+++ controllers/bg-translate.js
@@ -1,184 +1,75 @@
 resumeApp.controller("mainCtrlBg", mainCtrlBg);
 
 
-function mainCtrlBg($scope) {
+function mainCtrlBg($scope, $http) {
 	'use strict';
-	$scope.personalBg = [
-		{
-			"1. Име:": "Евгени Георгиев Василев"
-				},
-		{
-			"2. Местоживеене: ": "Град Перник"
-				},
-		{
-			"3. GSM: ": "0898/ 93-81-93"
-				},
-		{
-			"4. Електронна Поща: ": "evgeni.vasilev0912gmail.com"
-				}
-            ];
 
-	$scope.educationBg = [
-		{
-			"1. Висше Образование: ": "Магистърска Степен по Психология; Софийски Университет 'Свети Климент Охридски'"
-				},
-		{
-			"2. Средно Образование: ": "Природо-математическа Гимназия  - 'Христо Смирненски' - Перник"
-				},
-		{
-			"3. Сертификати и Дипломи:  ": "Сертифициран Майкрософт Специалист: »»  1. Сертификационнен Номер : E842-7422 »»    2. Сертификация/Версия: Програмиране на HTML5 със JavaScript и CSS3   Специалист"
-				}
-            ];
+	// personal
+	var promice_personal = $http.get("data_bg/personal.json");
+	promice_personal.success(function (data) {
+		$scope.personal_bg = data;
+	});
 
-	$scope.experienceBg = [
-		{
-			"1. Авиационна Сигурност: ": "Аерогара София – 2002/2009"
-				},
-		{
-			"2.Ефирен Техник и Уеб Администратор: ": "Локална Кабелна Телевизия: Кракра – Перник 2009/2011"
-				},
-		{
-			"3.Уеб Разработчик на Свободна Практика: ": "2011 – 2013"
-				},
-		{
-			"4. Уеб Разработчик - временнен договор: ": "Web Factory 2013 – Юни/Юли"
-				},
-		{
-			"5. Уеб Разработчик на Свободна Практика: ": "2013 – Юли/ 2014 – Януари"
-				},
-		{
-			"6. PHP Уеб Разработчик ": "Support BG 2014 – Януар/Март"
-				},
-		{
-			"7. Водещ Фронт-енд Разработчик: ": "GDMFX: 2014 Август – 2015 Февруари"
-				},
-            ]
-	$scope.skillsBg = [
-		{
-			"1. Езици за Маркиране на Хипер-текст:  ": "HTML – 4.1, HTML – 5, XML"
-				},
-		{
-			"2. Езици за Прилагане на Каскадни Стилове: ": "CSS – 2.1, CSS – 3"
-				},
-		{
-			"3. Скриптове от Страна на Клиента : ": "JavaScript – Процедурен и ООП, AJAX jQuery, jQuery Mobile, DOM Scripting, JSON, Angular.js"
-				},
-		{
-			"4. Скриптове от Страна на Кклиента: ": "PHP – Процедурен и ООП, MVC Архитектура"
-				},
-		{
-			"5. СУБД: ": "MySql"
-				},
-		{
-			"6. Уеб Графика и Дизайн: ": "Macromedia Fireworks"
-				},
-		{
-			"7. Система за Контрол на Версиите: ": "GIT"
-				},
-		{
-			"8. Комплексни Фронт-енд Шаблони: ": "Twitter Bootstrap"
-				},
-		{
-			"9. Други Технологии: ": "Sass, Apache Cordova, Vagrant, Flash, Drupal"
-				},
-		{
-			"10. Интегрирани Среди за Разработка: ": "PHP Storm, NetBeans, Visual Studio"
-				},
-		{
-			"11. ОС:": "Windows 8.1 (предпочитана), Linux – Ubuntu Дистрибуция"
-				}
+	// education
+	var promice_education = $http.get("data_bg/education.json");
+	promice_education.success(function (data) {
+		$scope.education_bg = data;
+	});
 
-            ];
-	$scope.languagesBg = [
-		{
-			"Английски Език: ": "IELTS Certificate"
-				},
-		{
-			"Руски Език: ": "Говоримо, Четене"
-		}
-            ];
+	// experience
+	var promice_experience = $http.get("data_bg/experience.json");
+	promice_experience.success(function (data) {
+		$scope.experience_bg = data;
+	});
 
-	$scope.projectsBg = [
-		{
-			"http://www.be-web.bg": "www.be-web.bg"
-				},
-		{
-			"http://www.greenlifehotel-bansko.bg": "www.greenlifehotel-bansko.bg"
-				},
-		{
-			"http://www.gdmfx.com": "www.gdmfx.com"
-				},
-		{
-			"http://www.gdmfx.com/promo-binary-options": "www.gdmfx.com/promo-binary-options"
-				},
-		{
-			"http://www.gdmfx.com/demo-trading-account": "www.gdmfx.com/demo-trading-account"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-Single-Page-Sample/": "http://evgenivasilev.github.io/agular-Single-Page-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Geolocation-Sample/": "http://evgenivasilev.github.io/Geolocation-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Vanila-JS-Form-Validation/": "http://evgenivasilev.github.io/Vanila-JS-Form-Validation/"
-				},
-		{
-			"http://evgenivasilev.github.io/jQuery-Sample-for-Building-Simple-Equation-Test/": "http://evgenivasilev.github.io/jQuery-Sample-for-Building-Simple-Equation-Test/"
-				},
-		{
-			"http://evgenivasilev.github.io/Horizontal-Bar-Chart/": "http://evgenivasilev.github.io/Horizontal-Bar-Chart/"
-				},
-		{
-			"http://evgenivasilev.github.io/OOP-JavaSCript-Sample/": "http://evgenivasilev.github.io/OOP-JavaSCript-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-To-Do-Application/": "http://evgenivasilev.github.io/Angular-To-Do-Application/"
-				},
-		{
-			"http://evgenivasilev.github.io/Web-Form/": "http://evgenivasilev.github.io/Web-Form/"
-				},
-		{
-			"http://evgenivasilev.github.io/BPI/": "http://evgenivasilev.github.io/BPI/"
-				},
-		{
-			"http://evgenivasilev.github.io/Flash-Weather-Application/": "http://evgenivasilev.github.io/Flash-Weather-Application/"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-Form-Validation/": "http://evgenivasilev.github.io/Angular-Form-Validation/"
-				}
-            ]
+	// skills
+	var promice_skills = $http.get("data_bg/skills.json");
+	promice_skills.success(function (data) {
+		$scope.skills_bg = data;
+	});
+
+	// languages
+	var promice_languages = $http.get("data_bg/languages.json");
+	promice_languages.success(function (data) {
+		$scope.languages_bg = data;
+	});
+
+	// projects
+	var promice_projects = $http.get("data_en/projects.json");
+	promice_projects.success(function (data) {
+		$scope.projects_bg = data;
+	});
 }
 resumeApp.config(['$routeProvider',
             function ($routeProvider) {
 		$routeProvider.
 		when('/bg', {
-			templateUrl: 'partials/personals-bg.html',
+			templateUrl: 'partials/bg/personals-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
 		when('/education/bg', {
-			templateUrl: 'partials/education-bg.html',
+			templateUrl: 'partials/bg/education-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
 		when('/experience/bg', {
-			templateUrl: 'partials/experience-bg.html',
+			templateUrl: 'partials/bg/experience-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
 		when('/skills/bg', {
-			templateUrl: 'partials/skills-bg.html',
+			templateUrl: 'partials/bg/skills-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
 		when('/languages/bg', {
-			templateUrl: 'partials/languages-bg.html',
+			templateUrl: 'partials/bg/languages-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
 		when('/projects/bg', {
-			templateUrl: 'partials/projects-bg.html',
+			templateUrl: 'partials/bg/projects-bg.html',
 			controller: 'mainCtrlBg'
 
 		}).
--- controllers/main-ctrl.js
+++ controllers/main-ctrl.js
@@ -1,6 +1,7 @@
 resumeApp.controller("personalDetails", personalDetails);
 
-function personalDetails($scope, $window) {
+function personalDetails($scope, $window, $http) {
+	'use strict';
 	$scope.lng = "en";
 	$scope.resume = "Resume";
 	$scope.pDetails = "Personal Details";
@@ -49,149 +50,42 @@ function personalDetails($scope, $window) {
 		}
 
 	}
-	$scope.personal = [
-		{
-			"1. Full Name:": "Evgeni Georgiev  Vasilev"
-				},
-		{
-			"2. Address: ": "City of Pernik"
-				},
-		{
-			"3. GSM: ": "0898/ 93-81-93"
-				},
-		{
-			"4. E-mail: ": "evgeni.vasilev0912gmail.com"
-				}
-            ];
-	$scope.education = [
-		{
-			"1. Master Degree in Psychology: ": "University of Sofia – 'Snt. Climent Ohridski'"
-				},
-		{
-			"2. High School: ": "Nature and Mathematics - 'Hristo Smirnenski - Pernik'"
-				},
-		{
-			"3. Certification and Diplomas:  ": "Microsoft Specialist: »»  1. Certification Number : E842-7422 »»    2. Certification/Version: Programming in HTML5 with JavaScript and CSS3   Specialist"
-				}
-            ]
-	$scope.experience = [
-		{
-			"1. Aviation Security Officer: ": "Airport Sofia – 2002/2009"
-				},
-		{
-			"2. On-air Broadcast Technician and Web Site Administrator: ": "Local Cable TV Network – Pernik 2009/2011"
-				},
-		{
-			"3. Freelance Web Development: ": "2011 – 2013"
-				},
-		{
-			"4. Web Developer – term contract: ": "Web Factory 2013 – June/July"
-				},
-		{
-			"5. Freelance Web Development: ": "2013 – July/ 2014 – January"
-				},
-		{
-			"6. PHP Web Development: ": "Support BG 2014 – January – March"
-				},
-		{
-			"7. Senior Front-end Developer: ": "GDMFX: 2014 August – 2015 February"
-				},
-            ]
-	$scope.skills = [
-		{
-			"1. Hyper Text Markup  Languages:  ": "HTML – 4.1, HTML – 5, XML"
-				},
-		{
-			"2. Style Sheet Languages: ": "CSS – 2.1, CSS – 3"
-				},
-		{
-			"3. Client Side Scripts: ": "JavaScript – Procedural and OOP, AJAX jQuery, jQuery Mobile, DOM Scripting, JSON, Angular.js"
-				},
-		{
-			"4. Server Side Scripts: ": "PHP – Procedural and OOP, MVC Architecture"
-				},
-		{
-			"5. Database: ": "MySql"
-				},
-		{
-			"6. Web Graphics and Design: ": "Macromedia Fireworks"
-				},
-		{
-			"7. Version Control System: ": "GIT"
-				},
-		{
-			"8. Compound Front – end Frameworks: ": "Twitter Bootstrap"
-				},
-		{
-			"9. Other Technologies: ": "Sass, Apache Cordova, Vagrant, Flash, Drupal"
-				},
-		{
-			"10. Integrated Development Enviornment: ": "PHP Storm, NetBeans, Visual Studio"
-				},
-		{
-			"11. OS:": "Windows 8.1 (preferred), Linux – Ubuntu Distribution"
-				}
 
-            ];
-	$scope.languages = [
-		{
-			"English language: ": "IELTS Certificate"
-				},
-		{
-			"Russian Language: ": "Speak and read"
-				}
-            ];
-	$scope.projects = [
-		{
-			"http://www.be-web.bg": "www.be-web.bg"
-				},
-		{
-			"http://www.greenlifehotel-bansko.bg": "www.greenlifehotel-bansko.bg"
-				},
-		{
-			"http://www.gdmfx.com": "www.gdmfx.com"
-				},
-		{
-			"http://www.gdmfx.com/promo-binary-options": "www.gdmfx.com/promo-binary-options"
-				},
-		{
-			"http://www.gdmfx.com/demo-trading-account": "www.gdmfx.com/demo-trading-account"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-Single-Page-Sample/": "http://evgenivasilev.github.io/agular-Single-Page-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Geolocation-Sample/": "http://evgenivasilev.github.io/Geolocation-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Vanila-JS-Form-Validation/": "http://evgenivasilev.github.io/Vanila-JS-Form-Validation/"
-				},
-		{
-			"http://evgenivasilev.github.io/jQuery-Sample-for-Building-Simple-Equation-Test/": "http://evgenivasilev.github.io/jQuery-Sample-for-Building-Simple-Equation-Test/"
-				},
-		{
-			"http://evgenivasilev.github.io/Horizontal-Bar-Chart/": "http://evgenivasilev.github.io/Horizontal-Bar-Chart/"
-				},
-		{
-			"http://evgenivasilev.github.io/OOP-JavaSCript-Sample/": "http://evgenivasilev.github.io/OOP-JavaSCript-Sample/"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-To-Do-Application/": "http://evgenivasilev.github.io/Angular-To-Do-Application/"
-				},
-		{
-			"http://evgenivasilev.github.io/Web-Form/": "http://evgenivasilev.github.io/Web-Form/"
-				},
-		{
-			"http://evgenivasilev.github.io/BPI/": "http://evgenivasilev.github.io/BPI/"
-				},
-		{
-			"http://evgenivasilev.github.io/Flash-Weather-Application/": "http://evgenivasilev.github.io/Flash-Weather-Application/"
-				},
-		{
-			"http://evgenivasilev.github.io/Angular-Form-Validation/": "http://evgenivasilev.github.io/Angular-Form-Validation/"
-				}
-            ]
+	// personal details
+	var promice_personal = $http.get("data_en/personals.json");
+	promice_personal.success(function (data) {
+		$scope.personal = data;
+	});
 
+	// education
+	var promice_education = $http.get("data_en/education.json");
+	promice_education.success(function (data) {
+		$scope.education = data;
+	});
+
+	// work experience	
+	var promice_experience = $http.get("data_en/experience.json");
+	promice_experience.success(function (data) {
+		$scope.experience = data;
+	});
+
+	// skills
+	var promice_skills = $http.get("data_en/skills.json");
+	promice_skills.success(function (data) {
+		$scope.skills = data;
+	});
+
+	// languages
+	var promice_languages = $http.get("data_en/languages.json");
+	promice_languages.success(function (data) {
+		$scope.languages = data;
+	});
+
+	// projects
+	var promice_projects = $http.get("data_en/projects.json");
+	promice_projects.success(function (data) {
+		$scope.projects = data;
+	});
 }
 
 
@@ -200,22 +94,22 @@ resumeApp.config(['$routeProvider',
 		'use strict';
 		$routeProvider.
 		when('/en', {
-			templateUrl: 'partials/personals.html'
+			templateUrl: 'partials/en/personals.html'
 		}).
 		when('/education/en', {
-			templateUrl: 'partials/education.html'
+			templateUrl: 'partials/en/education.html'
 		}).
 		when('/experience/en', {
-				templateUrl: "partials/experience.html"
+				templateUrl: "partials/en/experience.html"
 			})
 			.when('/skills/en', {
-				templateUrl: "partials/skills.html"
+				templateUrl: "partials/en/skills.html"
 			}).
 		when('/languages/en', {
-			templateUrl: "partials/languages.html"
+			templateUrl: "partials/en/languages.html"
 		}).
 		when('/projects/en', {
-			templateUrl: 'partials/projects.html'
+			templateUrl: 'partials/en/projects.html'
 		}).
 		otherwise({
 			redirectTo: '/en'
